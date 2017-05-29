39234ea4eae0655de69af0700b0f6eefe7092306
NikusX <tigrnik3@gmail.com>
2017-05-30 01:07:29 +0300

    add out

--- ImageViewer/ImageViewer/Form1.Designer.cs
+++ ImageViewer/ImageViewer/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(875, 510);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(875, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(875, 535);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Просмотр изображений";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- ImageViewer/ImageViewer/Form1.cs
+++ ImageViewer/ImageViewer/Form1.cs
@@ -16,5 +16,44 @@ namespace ImageViewer
         {
             InitializeComponent();
         }
+
+        float[] fltpxl;
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                Bitmap bmpVvod = new Bitmap(openFileDialog1.OpenFile());
+                Color pxlclr1 = new Color();
+                fltpxl = new float[bmpVvod.Width * bmpVvod.Height * 4];
+                int count1 = 0;
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    for (int x = 0; x < pictureBox1.Width; x++)
+                    {
+                        pxlclr1 = bmpVvod.GetPixel(x, y);
+                        fltpxl[count1] = pxlclr1.A;
+                        fltpxl[count1 + 1] = pxlclr1.R;
+                        fltpxl[count1 + 2] = pxlclr1.G;
+                        fltpxl[count1 + 3] = pxlclr1.B;
+                        count1 += 4;
+                    }
+                }
+                Bitmap bmpVivod = new Bitmap(bmpVvod.Width, bmpVvod.Height);
+                Color pxlclr2;
+                int count2 = 0;
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    for (int x = 0; x < pictureBox1.Width; x++)
+                    {
+                        pxlclr2 = Color.FromArgb((int)fltpxl[count2], (int)fltpxl[count2 + 1], (int)fltpxl[count2 + 2], (int)fltpxl[count2 + 3]);
+                        bmpVivod.SetPixel(x, y, pxlclr2);
+                        count2 += 4;
+                    }
+                }
+                pictureBox1.Image = bmpVivod;
+            }
+        }
     }
 }
